aeff6c425e35e48c262ec6915a3b1aba1c9000db
shivam-thakkar <55989024+shivam-thakkar@users.noreply.github.com>
2019-09-30 19:57:27 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# MODERATOR
-GDFVNCH
+
+> MassDM v3, a bot to send DMs to every member of a role in Discord server.
+
+
+**Use this tool at your own risk!**
+
+You will need two things installed to make this bot work.
+
+- Node.JS(LTS version). https://nodejs.org/en/
+- A code editor. https://notepad-plus-plus.org/
+
+
+> Open `config.json` in your code editor and edit the token with your bot token or your user token. [or remove the "REM" and replate "YOUR TOKEN" in start.bat]
+
+You also need to add in your whitelisted ID. The ID of the user who will control the bot.
+Once you have that done, save the file.
+*Alternatively, you can pass in the token through an enviornment variable named `BOT_TOKEN`*
+
+
+> Run `install_dep.bat` if you are on Windows.
+
+Otherwise you need to open a terminal in the bot folder and type "npm install -d" without quotes.
+
+Once it has finished installing all the dependencies, you can run `start.bat`.
+*If you are not running Windows, you need to open a terminal in the bot folder and type "node app.js" without quotes.*
+
+
+
+Ideas for development:
+- [x] env var token
+- [ ] customize bot status
---
+++ app.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const figlet = require('figlet');
+const colors = require('colors');
+const readline = require('readline');
+const commando = require(`discord.js-commando`);
+
+const config = require('./config.json');
+const bot = new commando.Client({
+    commandPrefix:'mass!'
+});
+
+const cmdsArray = [
+    "dmall <message>",
+    "dmrole <role> <message>"
+];
+
+bot.on("ready", () => {
+    clear();
+    console.log('______')
+});
+
+
+bot.on("error", (error) => {
+    bot.login(config.token);
+});
+
+bot.registry.registerGroup('dms', 'help');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+if (process.env.BOT_TOKEN) bot.login(process.env.BOT_TOKEN);
+else bot.login(config.token);
+
+
+
+
+function clear() {
+    console.clear();
+    console.log(figlet.textSync("MassDM v3.3.0").green);
+    console.log("\n\nMass DM bot for Discord. \n Sends DMs to selected members of guild.\n  Forked and improved by Alex.");
+    console.log(`\nRandom send time set @ 0.01-${config.wait}s`);
+    console.log(` Type  ${config.prefix}help  in a chat.\n\n`);
+}
